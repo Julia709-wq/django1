e26e5f627b502b1468d96aea345ed21d27b3d8d6
Julia <yulia.2018.ivanova@gmail.com>
2025-09-15 01:12:38 +0300

    hw 24 done

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from . models import Product
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context=context)
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product': product
+    }
+    return render(request, 'product_detail.html', context=context)
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context=context)
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
