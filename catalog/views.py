9c96e04405fe1f72774963e46231997e57a40e4c
Julia <yulia.2018.ivanova@gmail.com>
2025-10-22 23:09:36 +0300

    [UPD] cash added, redis installed, services.py added

--- catalog/views.py
+++ catalog/views.py
@@ -5,8 +5,13 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponseForbidden
 
-from . models import Product
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+
+from . models import Product, Category
 from .forms import ProductForm
+from .services import ProductService
 
 
 def contacts(request):
@@ -21,6 +26,14 @@ def home(request):
 class ProductsListView(ListView):
     model = Product
 
+    # def get_queryset(self):  # низкоуровневое кэширование
+    #     queryset = cache.get('products_queryset')
+    #     if not queryset:
+    #         queryset = super().get_queryset()
+    #         cache.set('products_queryset', queryset, 60 * 15)
+    #     return queryset
+
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
 
@@ -73,3 +86,13 @@ class UnpublishProductView(View):
         product.is_published = False
         product.save()
         return redirect('catalog:home')
+
+
+def products_by_category_view(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = ProductService.get_by_category(category_id)
+
+    return render(request, 'catalog/category_products.html', {
+        'category': category,
+        'products': products
+    })
